2f0cec13ff19cfb17eec93b864e91e95789556b5
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2023-06-03 18:16:28 +0700

    Добавлена БД и работа с ней

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"html/template"
 	"net/http"
 )
 
+type User struct {
+	Id   uint   `json:"id"`
+	Name string `json:"name"`
+	Age  uint   `json:"age"`
+}
+
 func main() {
-	handleRequests()
+	// handleRequests()
+	testDB()
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -44,5 +53,48 @@ func handleRequests() {
 	mux.HandleFunc("/", homePage)
 	mux.HandleFunc("/contacts", contactsPage)
 
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":80", mux)
+}
+
+func assert(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func testDB() {
+	fmt.Println("Test")
+
+	const config string = "root:==PaSsWoRd==@tcp(127.0.0.1:3306)/main_database"
+
+	db, err := sql.Open("mysql", config)
+	assert(err)
+	defer db.Close()
+
+	create, err := db.Query(`CREATE TABLE IF NOT EXISTS users (
+        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        name VARCHAR(255) NOT NULL,
+		age INT NOT NULL,
+		PRIMARY KEY (id)
+    ) ENGINE = InnoDB`)
+	assert(err)
+	defer create.Close()
+
+	// insert, err := db.Query(`INSERT INTO users (name, age) VALUES ("Иван", 23)`)
+	// assert(err)
+	// defer insert.Close()
+
+	res, err := db.Query(`SELECT * FROM users`)
+	assert(err)
+
+	for res.Next() {
+		var user User
+
+		err = res.Scan(&user.Id, &user.Name, &user.Age)
+		assert(err)
+
+		fmt.Printf("Name: %s, age: %d, id %d\n", user.Name, user.Age, user.Id)
+	}
+
+	fmt.Println("Подключено к MySQL!!!")
 }
